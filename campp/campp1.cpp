1e90d94aa6154879be4cd236848719ae96d337ea
Kshitij <kshitijjain87199@gmail.com>
2023-07-02 20:04:46 +0530

    New commit

---
+++ campp/campp1.cpp
@@ -0,0 +1,23 @@
+// print n number of array in reverse
+// #include <cmath>
+// #include <cstdio>
+// #include <vector>
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for(int i=1; i<=n; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+//     for (int i = n; i>=1 ; i--)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+    
+//     return 0;
+// }
---
+++ campp/campp3.cpp
@@ -0,0 +1,16 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// struct workshop
+// {
+//     char f_name[100];
+//     char l_name[100];
+//     int sal;
+// };
+// int main(){
+// workshop wk;
+//     cin>>wk.f_name;
+//     cin>>wk.l_name;
+//     cin>>wk.sal;
+//     cout<<wk.f_name<<" "<<wk.l_name<<" "<<wk.sal;
+//     return 0;
+// }
